0e7a5d9e980a19182a2ac9237404f16c9038767d
Akhilak6789 <kodalikodali99@gmail.com>
2024-06-28 12:28:23 -0500

    Committing StringEx.java

--- FirstProgram/StringEx.java
+++ FirstProgram/StringEx.java
@@ -3,5 +3,6 @@ package FirstProgram;
 public class StringEx {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("Hope you are doing good!!");
     }
 }

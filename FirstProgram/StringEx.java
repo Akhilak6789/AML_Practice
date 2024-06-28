61df3ef74ca7650403e7d93ce5c08505a1297b41
Akhilak6789 <kodalikodali99@gmail.com>
2024-06-28 13:57:57 -0500

    Adding Hope you are doing good in stringEx.java

--- FirstProgram/StringEx.java
+++ FirstProgram/StringEx.java
@@ -2,6 +2,7 @@ package FirstProgram;
 
 public class StringEx {
     public static void main(String[] args) {
+        System.out.println("Hope you are doing good!!");
         System.out.println("Hello World!!!");
     }
 }
